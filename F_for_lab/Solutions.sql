17559cc0142130d17469336c7ca0c461e5a843b8
jecastrom <jcastro.de@outlook.com>
2022-01-18 23:51:17 +0100

    s

--- F_for_lab/Solutions.sql
+++ F_for_lab/Solutions.sql
@@ -1,8 +1,11 @@
 SELECT
     concat((last_name), ', ', (first_name)) AS staff_member_name,
-    address
+    sum(amount) AS total_sales_august_2005
 FROM
     staff s
-    INNER JOIN address a ON s.address_id = a.address_id
-ORDER BY
+    INNER JOIN payment p ON s.staff_id = p.staff_id
+WHERE
+    monthname(payment_date) = 'August'
+    AND year(payment_date) = 2005
+GROUP BY
     1;
